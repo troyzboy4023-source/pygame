70f32f2beac3c9778cb59ac48fa43e171003b0d7
BTBN0 <baysaabataa77@gmail.com>
2025-10-22 20:03:41 +0800

    baysaa map

---
+++ boss_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boss_1" tilewidth="32" tileheight="32" tilecount="2304" columns="48">
+ <image source="../boss 1/boss_1.png" width="1536" height="1536"/>
+</tileset>
---
+++ boss_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boss_3" tilewidth="32" tileheight="32" tilecount="1872" columns="52">
+ <image source="../boss 3/boss_3.png" width="1680" height="1152"/>
+</tileset>
